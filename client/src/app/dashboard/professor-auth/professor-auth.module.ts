80ca2d12c1e66007f3f0e03f5106970b988ecc99
Sumedhabelur <sumedhabelur@gmail.com>
2020-01-26 18:23:09 +0530

    Professor Login Component Added

--- client/src/app/dashboard/professor-auth/professor-auth.module.ts
+++ client/src/app/dashboard/professor-auth/professor-auth.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfessorAuthRoutingModule } from './professor-auth-routing.module';
+import { ProfessorLoginComponent } from './professor-login/professor-login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfessorLoginComponent],
   imports: [
     CommonModule,
     ProfessorAuthRoutingModule
