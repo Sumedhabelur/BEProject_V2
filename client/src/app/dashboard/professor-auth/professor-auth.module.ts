768a6e979ff230c2599753bbd3615845b2419f80
Sumedhabelur <sumedhabelur@gmail.com>
2020-01-26 18:18:47 +0530

    professor-auth module added

---
+++ client/src/app/dashboard/professor-auth/professor-auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfessorAuthRoutingModule } from './professor-auth-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProfessorAuthRoutingModule
+  ]
+})
+export class ProfessorAuthModule { }
