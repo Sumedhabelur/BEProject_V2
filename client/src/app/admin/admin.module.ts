a3e0e55db3f1b59913e8eca11c9b2a029c3a0656
SiddhiHinge <hingesiddhi55@gmail.com>
2020-01-31 20:24:53 +0530

    student-list comp added in admin

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -6,13 +6,15 @@ import { AdminComponent } from './admin.component';
 import { StudentRegistrationFormComponent } from './student-registration-form/student-registration-form.component';
 import { ProfessorRegistrationFormComponent } from './professor-registration-form/professor-registration-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StudentListComponent } from './student-list/student-list.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     StudentRegistrationFormComponent,
-    ProfessorRegistrationFormComponent
+    ProfessorRegistrationFormComponent,
+    StudentListComponent
   ],
   imports: [
     CommonModule,
