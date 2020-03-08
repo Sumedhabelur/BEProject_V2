40085f58c32868f4b7d2f1e3fd4df950182c1e6d
Pritam Kamble <7276279026.pk@gmail.com>
2020-03-08 11:58:18 +0530

    auth guard added for Admin module

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { AdminGuard } from './admin-guard/admin.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: AdminComponent }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full', canActivate: [AdminGuard] },
+  { path: 'dashboard', component: AdminComponent, canActivate: [AdminGuard] }
 ];
 
 @NgModule({
