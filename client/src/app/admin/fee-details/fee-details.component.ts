d62cb206c14d2a66e1b3c1f1c5305a9248c3f361
SiddhiHinge <hingesiddhi55@gmail.com>
2020-09-13 20:05:11 +0530

    Routes added for get,view,update fees in admin

--- client/src/app/admin/fee-details/fee-details.component.ts
+++ client/src/app/admin/fee-details/fee-details.component.ts
@@ -9,78 +9,11 @@ import { AdminService } from '../service/admin.service';
 })
 export class FeeDetailsComponent implements OnInit {
 
-  feeForm: FormGroup;
-  students = [];
-  addFeeForm: FormGroup;
-  isRegisterFailed = false;
-  isViewForm = false;
-  studentId;
+  type = 'add';
 
   constructor(
-    private fb: FormBuilder,
-    private adminService: AdminService
-  ) { }
+) { }
 
-  ngOnInit() {
-    this.buildForm();
-  }
-
-  buildForm() {
-    this.feeForm = this.fb.group({
-      class: ['SE', Validators.required],
-    });
-
-    this.addFeeForm = this.fb.group({
-      userName: ['', Validators.required],
-      payment1: ['', Validators.required],
-      date1: ['', Validators.required],
-      payment2: ['', Validators.required],
-      date2: ['', Validators.required],
-      totalFee: ['', Validators.required],
-      balanceFee: ['', Validators.required]
-    });
-  }
-
-  onClassSelect() {
-    const className = this.feeForm.get('class').value;
-    this.getStudentByClass(className);
-  }
-
-  getStudentByClass(data) {
-    this.adminService.getStudentByClass(data).subscribe((response: any) => {
-      this.students = response.result;
-    });
-    console.log(this.students);
-  }
-
-  addStudentFee(student) {
-    console.log(student._id);
-    this.studentId = student._id;
-    this.isViewForm = true;
-    this.addFeeForm.get('userName').setValue(student.userName);
-  }
-
-  onRegisterClick() {
-    console.log('In register Fee !!');
-    const data = {
-      studentId: this.studentId,
-      class: this.feeForm.get('class').value,
-      payment1: this.addFeeForm.get('payment1').value,
-      date1: this.addFeeForm.get('date1').value,
-      payment2: this.addFeeForm.get('payment2').value,
-      date2: this.addFeeForm.get('date2').value,
-      totalFee: this.addFeeForm.get('totalFee').value,
-      balanceFee: this.addFeeForm.get('balanceFee').value,
-    };
-    this.adminService.registerFees(data).subscribe((response: any) => {
-      console.log(response);
-      if (response) {
-        console.log('Successful');
-      } else {
-        this.isRegisterFailed = true;
-      }
-
-    });
-  }
+  ngOnInit() {}
 
 }
